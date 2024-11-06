ead23bf49af1285ff26731aba643166d0eb72b78
atiixx <jonas.hauenstein@outlook.de>
2024-11-06 15:15:44 +0100

    added client amount info

--- index.js
+++ index.js
@@ -4,10 +4,20 @@ const wss = new WebSocketServer({ port: 8080 });
 
 console.log("Server listening on: ", wss.address());
 
-wss.on("connection", function connection(ws, req) {
-  console.log(wss.clients);
-  console.log("Anzahl Clients: ", wss.clients.size);
+wss.on("connection", function connection(ws) {
+  console.log(
+    "Client opened connection. Number of clients: ",
+    wss.clients.size
+  );
+
   ws.on("error", console.error);
+
+  ws.on("close", function close() {
+    console.log(
+      "Client closed connection. Number of clients: ",
+      wss.clients.size
+    );
+  });
 
   ws.on("message", function message(data, isBinary) {
     wss.clients.forEach(function each(client) {
